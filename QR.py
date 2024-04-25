e1e4a4fa3d1dc516508055898c2592608f279297
Vishal Kumar <133562727+VishalRock04@users.noreply.github.com>
2024-04-25 19:05:22 +0530

    Add files via upload

---
+++ QR.py
@@ -0,0 +1,21 @@
+import qrcode
+
+
+def generate_qr_code(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="red", back_color="blue")
+    img.save(filename)
+
+
+data = "https://www.youtube.com/"
+filename = "SBI.png"
+generate_qr_code(data, filename)
+print(f"QR code generated and saved as {filename}")
